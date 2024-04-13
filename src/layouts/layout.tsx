6df8878830b4b9cc473adf7655f6b1152d2dea49
madhushani-29 <madhushani2904@gmail.com>
2024-04-13 16:08:46 +0530

    header

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 
@@ -15,6 +16,7 @@ const Layout = ({ children }: Props) => {
             <Header />
             <Hero/>
             <div className="container mx-auto flex-1 py-10">{children}</div>
+            <Footer/>
         </div>
     )
 }

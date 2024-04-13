7e99c16d938aae8d6ae2d27e56d895614d058b54
madhushani-29 <madhushani2904@gmail.com>
2024-04-13 15:20:16 +0530

    home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import MainNav from './MainNav';
 const Header = () => {
     return (
         <div className='border-b-2 border-b-orange-500'>
-            <div className='container mx-auto flex justify-between items-center'>
+            <div className='container mx-auto flex justify-between items-center py-6'>
                 <Link to="/" className='text-3xl font-bold tracking-tight text-orange-500'>MernEats.com</Link>
                 <div className='md:hidden'>
                     <MobileNav/>

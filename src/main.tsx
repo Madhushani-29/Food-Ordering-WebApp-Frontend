6cc4e8f98c905582c5529bdb33112a2c88206f9f
madhushani-29 <madhushani2904@gmail.com>
2024-04-23 22:27:32 +0530

    user create api

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,29 @@ import './global.css'
 import { BrowserRouter as Router } from "react-router-dom"
 import AppRoutes from './AppRoutes'
 import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate'
+import { QueryClient, QueryClientProvider } from "react-query";
 
+//query client-central component in the react-query library responsible for managing cache, data fetching, and mutation operations
+const queryClient = new QueryClient({
+  defaultOptions: {
+    //configure default options specifically related to query operations
+    queries: {
+      //This is a configuration option for queries, 
+      //which determines whether to automatically refetch data when the window gains focus. 
+      refetchOnWindowFocus: false,
+    }
+  }
+});
+
+//query client provider can use anywhere in the app since it has wrap the full application
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
-      <Auth0ProviderWithNavigate>
-        <AppRoutes />
-      </Auth0ProviderWithNavigate>
+      <QueryClientProvider client={queryClient}>
+        <Auth0ProviderWithNavigate>
+          <AppRoutes />
+        </Auth0ProviderWithNavigate>
+      </QueryClientProvider>
     </Router>
   </React.StrictMode>,
 )

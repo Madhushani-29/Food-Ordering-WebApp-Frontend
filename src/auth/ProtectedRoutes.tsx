370ed7073bf14668578b6580e1146cef2b4ea76c
madhushani-29 <madhushani2904@gmail.com>
2024-05-02 11:30:41 +0530

    protected routes changed

--- src/auth/ProtectedRoutes.tsx
+++ src/auth/ProtectedRoutes.tsx
@@ -2,13 +2,18 @@ import { useAuth0 } from '@auth0/auth0-react'
 import { Navigate, Outlet } from 'react-router-dom';
 
 const ProtectedRoutes = () => {
-    const { isAuthenticated } = useAuth0();
+    const { isAuthenticated, isLoading } = useAuth0();
 
     //if the user is authenticated, the child routes will be rendered
     //sAuthenticated is false, the component returns a Navigate component with the to prop set to "/" and replace
-    return isAuthenticated ?
-        <Outlet /> :
-        <Navigate to="/" replace />
-}
+    //if dont use loading, it will not wait for authenticated or not, it will directly navigate to /
+    if (isLoading) {
+        return null;
+    }
+    if (isAuthenticated) {
+        return <Outlet />;
+    }
+    return <Navigate to="/" replace />;
+};
 
 export default ProtectedRoutes;

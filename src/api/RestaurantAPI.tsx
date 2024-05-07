06f241074d56a6e838eefbedfc13213c1d10faa8
madhushani-29 <madhushani2904@gmail.com>
2024-05-07 20:51:48 +0530

    restaurant search with cuisines or name

--- src/api/RestaurantAPI.tsx
+++ src/api/RestaurantAPI.tsx
@@ -1,3 +1,4 @@
+import { SearchState } from "@/pages/SearchPage";
 import { RestaurantSearchResponse } from "@/types";
 import { useQuery } from "react-query";
 
@@ -5,9 +6,11 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 //there get parameters in the method out of the hook since
 //the city parameter need to use outside of the hook also 
-export const useSearchRestaurants = (city?: string) => {
+export const useSearchRestaurants = (searchState: SearchState, city?: string) => {
     const createSearchRequest = async (): Promise<RestaurantSearchResponse> => {
-        const response = await fetch(`${API_BASE_URL}/api/restaurant/search/${city}`, {
+        const params = new URLSearchParams();
+        params.set("searchQuery", searchState.searchQuery);
+        const response = await fetch(`${API_BASE_URL}/api/restaurant/search/${city}?${params.toString()}`, {
             method: "GET",
         });
 
@@ -24,7 +27,8 @@ export const useSearchRestaurants = (city?: string) => {
             //in other ways, if there are not no of params then cat fetch data once and can use as "searchRestaurants" with out []
             //If the parameters or dependencies within this array change, React Query knows to invalidate the cache and fetch fresh data
             //array allows for more flexibility, especially when you have multiple parameters or dependencies that affect the data fetching process
-            ["searchRestaurants"],
+            //searchState is a dynamic object representing the current state of the search
+            ["searchRestaurants", searchState],
             createSearchRequest,
             //call createSearchRequest when only city is not null
             { enabled: !!city }

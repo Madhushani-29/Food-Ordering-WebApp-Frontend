f0fa023fc225f1ed9783eeaf496b158af02c5d08
madhushani-29 <madhushani2904@gmail.com>
2024-05-08 20:21:46 +0530

    restaurant page contect create

--- src/pages/RestaurantDetailsPage.tsx
+++ src/pages/RestaurantDetailsPage.tsx
@@ -1,5 +1,7 @@
 import { useGetRestaurantById } from "@/api/RestaurantAPI";
 import { useParams } from "react-router-dom";
+import { AspectRatio } from "../components/ui/aspect-ratio";
+import RestaurantInfo from "@/components/RestaurantInfo";
 
 const RestaurantDetailsPage = () => {
     const { id } = useParams();
@@ -14,8 +16,24 @@ const RestaurantDetailsPage = () => {
     }
 
     return (
-        <div>RestaurantDetailsPage: {id}
-            <div>{restaurant.restaurantName}</div>
+        <div className="flex flex-col gap-10">
+            <AspectRatio ratio={16 / 5}>
+                <img
+                    src={restaurant.imageUrl}
+                    className="rounded-md object-cover h-full w-full"
+                />
+            </AspectRatio>
+            <div className="grid md:grid-cols-[4fr_2fr] gap-5 md:px-32">
+                <div className="flex flex-col gap-4">
+                    <RestaurantInfo restaurant={restaurant} />
+                    <span className="text-2xl font-bold tracking-tight">Menu</span>
+                    
+                </div>
+                <div>
+                    Cart
+                </div>
+            </div>
+
         </div>
     )
 }

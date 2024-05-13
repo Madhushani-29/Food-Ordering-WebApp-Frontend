723ac3bc0a9fcf0e8f39624a5146fba1a4d21c9f
madhushani-29 <madhushani2904@gmail.com>
2024-05-13 23:12:26 +0530

    manage restaurant page

--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,10 +1,13 @@
-import { useCreateMyRestaurant, useGetMyRestaurant, useUpdateMyRestaurant } from "@/api/MyRestaurantAPI";
+import { useCreateMyRestaurant, useGetMyRestaurant, useGetMyRestaurantOrders, useUpdateMyRestaurant } from "@/api/MyRestaurantAPI";
+import OrderItemCard from "@/components/OrderItemCard";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
 const ManageRestaurantPage = () => {
   const { createRestaurant, isLoading: isCreateLoading } = useCreateMyRestaurant();
   const { currentRestaurant, isLoading: isGetLoading } = useGetMyRestaurant();
   const { updateRestaurant, isLoading: isUpdateLoading } = useUpdateMyRestaurant();
+  const { restaurantOrders, isLoading: isGetOrdersLoading } = useGetMyRestaurantOrders();
 
   // '!!' operator is a double negation, which coerces a value to its boolean equivalent
   // When currentRestaurant is falsy (null, undefined, 0, false, or an empty string), 
@@ -16,10 +19,32 @@ const ManageRestaurantPage = () => {
   }
 
   return (
-    <ManageRestaurantForm
-      onSave={isEditing ? updateRestaurant : createRestaurant}
-      isLoading={isCreateLoading || isUpdateLoading}
-      currentRestaurant={currentRestaurant} />
+    <Tabs defaultValue="orders">
+      <TabsList>
+        <TabsTrigger value="orders">
+          Orders
+        </TabsTrigger>
+        <TabsTrigger value="manage-restaurant">
+          Manage Restaurant
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent
+        value="orders"
+        className="space-y-5 bg-gray-50 p-10 rounded-lg"
+      >
+        <h2 className="text-2xl font-bold">{restaurantOrders?.length} active orders</h2>
+        {restaurantOrders?.map((order) => (
+          <OrderItemCard order={order} />
+        ))}
+      </TabsContent>
+      <TabsContent value="manage-restaurant">
+        <ManageRestaurantForm
+          onSave={isEditing ? updateRestaurant : createRestaurant}
+          isLoading={isCreateLoading || isUpdateLoading}
+          currentRestaurant={currentRestaurant}
+        />
+      </TabsContent>
+    </Tabs>
   )
 }
 

d0f2ba41d3f3ac601023d5b66d6331126904d6cb
madhushani-29 <madhushani2904@gmail.com>
2024-05-13 18:06:48 +0530

    order status page created

--- src/pages/OrderStatusPage.tsx
+++ src/pages/OrderStatusPage.tsx
@@ -1,4 +1,16 @@
+import { useGetMyOrders } from "@/api/OrderAPI"
+
 const OrderStatusPage = () => {
+  const { orders, isLoading } = useGetMyOrders();
+
+  if (isLoading) {
+    <span>Loading...</span>
+  }
+
+  if (!orders) {
+    <span>No results found</span>;
+  }
+
   return (
     <div>OrderStatusPage</div>
   )

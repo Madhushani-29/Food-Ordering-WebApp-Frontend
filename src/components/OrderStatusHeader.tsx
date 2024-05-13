19cdce1c06acd6d203968a31908a2959d509046e
madhushani-29 <madhushani2904@gmail.com>
2024-05-13 20:17:22 +0530

    order status configurations added

--- src/components/OrderStatusHeader.tsx
+++ src/components/OrderStatusHeader.tsx
@@ -1,4 +1,6 @@
 import { Order } from "@/types";
+import { Progress } from "./ui/progress";
+import { ORDER_STATUS } from "@/config/order-status-config";
 
 type Props = {
     order: Order;
@@ -21,12 +23,23 @@ const OrderStatusHeader = ({ order }: Props) => {
         return `${hours}:${paddedMinutes}`;
     };
 
+    const getOrderStatusInfo = () => {
+        return (
+            //there o is a single element in the order status array
+            //check each one of ORDER_STATUS equal to order status it will return the label and progress related to it
+            //if no one equal then assume the order only placed (first value in ORDER_STATUS array)
+            ORDER_STATUS.find((o) => o.value === order.status) || ORDER_STATUS[0]
+        );
+    };
+
+
     return (
         <>
             <h1 className="text-4xl font-bold tracking-tighter flex flex-col gap-5 md:flex-row md:justify-between">
-                <span> Order Status: {order.status}</span>
+                <span> Order Status: {getOrderStatusInfo().label}</span>
                 <span> Expected by: {getExpectedDelivery()}</span>
             </h1>
+            <Progress className="animate-pulse" value={getOrderStatusInfo().progressValue} />
         </>
     );
 };
---
+++ src/config/order-status-config.ts
@@ -0,0 +1,20 @@
+import { OrderStatus } from "@/types";
+
+type OrderStatusInfo = {
+  label: string;
+  value: OrderStatus;
+  progressValue: number;
+};
+
+//based on the each value have a label and progress value
+export const ORDER_STATUS: OrderStatusInfo[] = [
+  { label: "Placed", value: "placed", progressValue: 0 },
+  {
+    label: "Awaiting Restaurant Confirmation",
+    value: "paid",
+    progressValue: 25,
+  },
+  { label: "In Progress", value: "inProgress", progressValue: 50 },
+  { label: "Out for Delivery", value: "outForDelivery", progressValue: 75 },
+  { label: "Delivered", value: "delivered", progressValue: 100 },
+];
